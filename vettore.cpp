5c1498287d8068e6b9757719260462a5f8888025
Nevia23 <bzaghetto23@gmail.com>
2022-10-25 14:56:07 +0200

    update

--- vettore.cpp
+++ vettore.cpp
@@ -7,3 +7,32 @@ Deve inoltre includere il costruttore di copia profonda, l’assegnazione profon
 distruzione profonda.
 */
 
+#include<iostream>
+
+class Vettore {
+private:
+    int* a;
+    unsigned int size;
+    //vettore vuoto IFF a == nummptr && size == 0   (IFF = if and only if)
+public:
+//blocchiamo la conversione da unsigned int => Vettore
+    explicit Vettore(unsigned dim = 0, int v = 0): a(dim == 0 ? nullptr : new int[dim]), size(dim) {
+        for(unsigned int k = 0; k < size; ++k) a[k] = v;
+    }
+    Vettore(const Vettore& v);
+    Vettore& operator=(const Vettore& v);
+    ~Vettore();
+
+    Vettore operator+(const Vettore& v) const {
+        Vettore w(size + v.size);
+        unsigned int k = 0;
+        for(; k < size; ++k) w.a[k] = a[k];
+        for(; k < size + v.size; ++k) w.a[k] = v.a[k - size];
+        return w;
+    }
+
+    void append(const Vettore& v) {
+        //*this + v oppure this->operator+(v)
+        *this = this->operator+(v); //assegnazione profonda
+    }
+};
